e4ef427a582749a0e0778528b7c34f510990fbc4
Leonardo Perachi <leoperachi@gmail.com>
2025-07-08 11:15:53 -0300

    Configurando BD

--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,23 @@
-import { fastify } from "fastify";
-import { fastifyCors } from "@fastify/cors";
+import { fastifyCors } from '@fastify/cors';
+import { fastify } from 'fastify';
 import {
   serializerCompiler,
   validatorCompiler,
   type ZodTypeProvider,
-} from "fastify-type-provider-zod";
-import { env } from "./env.ts";
+} from 'fastify-type-provider-zod';
+import { env } from './env.ts';
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
 app.register(fastifyCors, {
-  origin: "*",
+  origin: '*',
 });
 
 app.setSerializerCompiler(serializerCompiler);
 app.setValidatorCompiler(validatorCompiler);
-console.log("Server Running");
-app.get("/", () => {
-  console.log("Get Home");
+console.log('Server Running');
+app.get('/', () => {
+  console.log('Get Home');
 });
 
 app.listen({ port: Number(env.PORT) || 3333 }, (err, address) => {
