65d14a81cb2e4a3d6b505166631a245e0eadfa8f
Leonardo Perachi <leoperachi@gmail.com>
2025-07-07 23:14:11 -0300

    Commit Inicial

---
+++ src/server.ts
@@ -0,0 +1,29 @@
+import { fastify } from "fastify";
+import { fastifyCors } from "@fastify/cors";
+import {
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
+} from "fastify-type-provider-zod";
+import { env } from "./env.ts";
+
+const app = fastify().withTypeProvider<ZodTypeProvider>();
+
+app.register(fastifyCors, {
+  origin: "*",
+});
+
+app.setSerializerCompiler(serializerCompiler);
+app.setValidatorCompiler(validatorCompiler);
+console.log("Server Running");
+app.get("/", () => {
+  console.log("Get Home");
+});
+
+app.listen({ port: Number(env.PORT) || 3333 }, (err, address) => {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+  console.log(`Server is running on ${address}`);
+});
