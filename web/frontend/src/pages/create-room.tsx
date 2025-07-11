88ef502ad93546179bc7f0a41c3b33d825564fd3
Leonardo Perachi <leoperachi@gmail.com>
2025-07-11 18:43:40 -0300

    Add Tailwind, biome

---
+++ web/frontend/src/pages/create-room.tsx
@@ -0,0 +1,7 @@
+import type React from "react";
+
+const CreateRoom: React.FC = () => {
+  return <div>Create Room Page</div>;
+};
+
+export default CreateRoom;
---
+++ web/frontend/src/pages/room.tsx
@@ -0,0 +1,7 @@
+import type React from "react";
+
+const Room: React.FC = () => {
+  return <div>Room Page</div>;
+};
+
+export default Room;
